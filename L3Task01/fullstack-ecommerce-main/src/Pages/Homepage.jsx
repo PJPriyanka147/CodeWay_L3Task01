46d8acda912fdb1803f30da36526c4777763a47e
PJPriyanka147 <priyankajatav07@gmail.com>
2024-03-14 14:40:27 +0530

    created featuredproduct

--- L3Task01/fullstack-ecommerce-main/src/Pages/Homepage.jsx
+++ L3Task01/fullstack-ecommerce-main/src/Pages/Homepage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import FeaturedProduct from '../components/FeaturedProduct/FeaturedProduct';
+import AllCategories from '../components/AllCategories/AllCategories';
 
 
 const Homepage = () => {
   return (
     <div className='hero'>Homepage
     <Hero/>
+    <FeaturedProduct/>
+    <AllCategories/>
       
     </div>
   )
