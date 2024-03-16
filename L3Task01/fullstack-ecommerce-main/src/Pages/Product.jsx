ed826fa5b9ada9156562b728c0c6f782762fb5bf
PJPriyanka147 <priyankajatav07@gmail.com>
2024-03-16 22:04:12 +0530

    created product page

--- L3Task01/fullstack-ecommerce-main/src/Pages/Product.jsx
+++ L3Task01/fullstack-ecommerce-main/src/Pages/Product.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../components/Breadcrums/Breadcrum.jsx';
+import { CategoriesContext } from '../components/Context/CatergoriesContext.jsx'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay.jsx';
 
-const Product = () => {
+
+const Product = (props) => {
+
+  const { all_product } = useContext(CategoriesContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId))
+
+
   return (
-    <div>Product</div>
+    
+    <div>
+       <Breadcrum product={product}/> 
+       <ProductDisplay product={product}/> 
+    </div>
   )
 }
 
