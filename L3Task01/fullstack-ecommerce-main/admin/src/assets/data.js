d08cded6d347a600c460fa32d9aad4a84bdb9d79
PJPriyanka147 <priyankajatav07@gmail.com>
2024-03-21 19:27:31 +0530

    update

---
+++ L3Task01/fullstack-ecommerce-main/admin/src/assets/data.js
@@ -0,0 +1,37 @@
+import product_1 from './product_1.png';
+import product_2 from './product_2.png';
+import product_3 from './product_3.png';
+import product_4 from './product_4.png';
+
+let dataProduct = [
+    {
+       id:1,
+       name: 'Black Mock Collar Puffer Jacket',
+       image: product_1,
+       new_price: 80.00,
+       old_price: 100.00,
+    },
+    {
+        id:2,
+        name: 'Plus Size Print Flared Sleeve Wrap Top',
+        image: product_2,
+        new_price: 30.00,
+        old_price: 80.00,
+     },
+     {
+        id:3,
+        name: 'Printed Mock Collar Sporty Jacket',
+        image: product_3,
+        new_price: 70.00,
+        old_price: 92.30,
+     },
+     {
+        id:4,
+        name: 'High Neck Long Sleeves Top',
+        image: product_4,
+        new_price: 20.00,
+        old_price: 10.50,
+     },
+]
+
+export default dataProduct
