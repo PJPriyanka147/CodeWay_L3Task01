085f899a7984e79d21f9cc1a781677142b27d0ca
PJPriyanka147 <priyankajatav07@gmail.com>
2024-03-22 13:13:18 +0530

    created routes

---
+++ L3Task01/fullstack-ecommerce-main/admin/src/components/AddProduct/AddProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AddProduct.css'
+
+const AddProduct = () => {
+  return (
+    <div className='add-product'>AddProduct</div>
+  )
+}
+
+export default AddProduct
