652728c3538935bc11aa29d846f8c5b5529bf8d4
PJPriyanka147 <priyankajatav07@gmail.com>
2024-04-01 15:03:15 +0530

    created sign up endpoint

--- L3Task01/fullstack-ecommerce-main/Backend/index.js
+++ L3Task01/fullstack-ecommerce-main/Backend/index.js
@@ -126,6 +126,59 @@ app.get('/allproducts', async (req, res)=>{
     res.send(products); 
 })
 
+//Schema for User Model
+
+const  Users = mongoose.model('Users',{
+    name :{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//Creating Endpoint for registering the user
+
+app.post('/Signup', async(req, res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if (check){
+        return res.status(400).json({successs:false, errros:"existing user found with same email address"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+       cart[i]=0;
+        
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+    
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+
+})
 
 app.listen(port, (error)=>{
     if(!error){
