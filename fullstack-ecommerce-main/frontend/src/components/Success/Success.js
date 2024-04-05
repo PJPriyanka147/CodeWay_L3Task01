ae9daa3eefe87c7177b09772b192d8462a33c3da
PJPriyanka147 <priyankajatav07@gmail.com>
2024-04-05 22:48:37 +0530

    created payment success

--- fullstack-ecommerce-main/frontend/src/components/Success/Success.js
+++ fullstack-ecommerce-main/frontend/src/components/Success/Success.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Success.css';
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <>
+    <div className='top-container'>
+    <div class="container">
+    <div class="printer-top"></div>
+
+    <div class="paper-container">
+        <div class="printer-bottom"></div>
+
+        <div class="paper">
+            <div class="main-contents">
+                <div class="success-icon">&#10004;</div>
+                <div class="success-title">
+                    Payment Complete
+                </div>
+                <div class="success-description">
+                    Thank you for completing the payment! You will shortly receive an email of your payment.
+                </div>
+                <div class="order-details">
+                    <div class="order-number-label">Transaction ID</div>
+                    <div class="order-number">123456789</div>
+                    <div class="complement">Thank You!</div>
+                </div>
+            </div>
+            <div class="jagged-edge"></div>
+        </div>
+    </div>
+</div>
+</div>
+</>
   )
 }
 
